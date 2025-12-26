cbc58161fe19e522b2e82d5e387f4e82c377e7a4
Krishna Dobhal <krishnadobhal234@gmail.com>
2025-12-26 21:38:38 +0530

    feat: Enhance RabbitMQ setup with delayed exchanges and dead letter queues

--- Notification/src/services/setupRabbitmq.ts
+++ Notification/src/services/setupRabbitmq.ts
@@ -3,24 +3,55 @@ import { rabbitMQ } from "@/client/rabbitmq.ts";
 export async function setupQueues() {
     const channel = rabbitMQ.getChannel();
 
-    // Main queue
-    await channel.assertQueue("Email-Queue", {
-        durable: true
+    await channel.assertExchange('dlx', 'direct', { durable: true });
+
+    await channel.assertExchange(
+        'notification-delayed-exchange',
+        'x-delayed-message',
+        {
+            durable: true,
+            arguments: {
+                'x-delayed-type': 'direct',
+            },
+        }
+    );
+
+    await channel.assertQueue('Email-Queue', {
+        durable: true,
+        arguments: {
+            'x-dead-letter-exchange': 'dlx',
+            'x-dead-letter-routing-key': 'email.dlq',
+        },
     });
 
-    await channel.assertQueue("SMS-Queue", {
-        durable: true
+    await channel.assertQueue('SMS-Queue', {
+        durable: true,
+        arguments: {
+            'x-dead-letter-exchange': 'dlx',
+            'x-dead-letter-routing-key': 'sms.dlq',
+        },
     });
 
-    // Dead Letter Queue
-    await channel.assertQueue("Email-DLQ", {
-        durable: true
-    });
+    await channel.assertQueue('Email-DLQ', { durable: true });
+    await channel.assertQueue('SMS-DLQ', { durable: true });
 
 
-    await channel.assertQueue("SMS-DLQ", {
-        durable: true
-    });
+    // Main queues (after delay)
+    await channel.bindQueue(
+        'Email-Queue',
+        'notification-delayed-exchange',
+        'email'
+    );
 
-    console.log("Queues ready");
+    await channel.bindQueue(
+        'SMS-Queue',
+        'notification-delayed-exchange',
+        'sms'
+    );
+
+    // DLQs
+    await channel.bindQueue('Email-DLQ', 'dlx', 'email.dlq');
+    await channel.bindQueue('SMS-DLQ', 'dlx', 'sms.dlq');
+
+    console.log('Queues + DLQ setup completed');
 }
